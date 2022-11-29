10c01bf32dc036f683cf1890455b594ecb6a0fcf
Niels Mouthaan <nielsmouthaan@me.com>
2022-11-29 10:12:57 +0100

    Created Xcode project

---
+++ ASO Suite (iOS)/Application.swift
@@ -0,0 +1,17 @@
+//
+//  Application.swift
+//  ASO Suite (iOS)
+//
+//  Created by Niels Mouthaan on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
