bbd533a8388cae93aa89e5f29b98924e85c01fdc
Niels Mouthaan <nielsmouthaan@me.com>
2022-11-29 15:02:59 +0100

    Add app class

---
+++ ASO Suite (iOS)/ASOSuite.swift
@@ -0,0 +1,17 @@
+//
+//  ASOSuite.swift
+//  ASO Suite (iOS)
+//
+//  Created by Niels Mouthaan on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ASOSuite: App {
+    var body: some Scene {
+        WindowGroup {
+            KeywordsView()
+        }
+    }
+}
